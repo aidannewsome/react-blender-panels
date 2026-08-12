804ab7e4811db574aa83c98065235092f98b963e
aidannewsome <aidan.newsome@me.com>
2026-08-12 16:08:35 -0400

    Gap the diagram panels and use the real npm mark

--- site/routes/__root.tsx
+++ site/routes/__root.tsx
@@ -65,8 +65,8 @@ function GitHubMark() {
 
 function NpmMark() {
 	return (
-		<svg viewBox="0 0 16 16" width="18" height="18" fill="currentColor" aria-hidden>
-			<path d="M0 3.5h16v9H8v-1.5H5.5v1.5H0v-9Zm1.5 1.5v6H4V6.5h1.5v4.5H7V5H1.5Zm7 0v6H11V6.5h1.5v4.5H14V5H8.5Z" />
+		<svg viewBox="0 0 24 24" width="18" height="18" fill="currentColor" aria-hidden>
+			<path d="M1.763 0C.786 0 0 .786 0 1.763v20.474C0 23.214.786 24 1.763 24h20.474c.977 0 1.763-.786 1.763-1.763V1.763C24 .786 23.214 0 22.237 0zM5.13 5.323l13.837.019-.009 13.836h-3.464l.01-10.382h-3.456L12.04 19.17H5.113z" />
 		</svg>
 	)
 }
